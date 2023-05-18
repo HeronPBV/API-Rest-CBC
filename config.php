5f330bb5219f9559bd16fec723fa96cf9ea2984d
HeronPBV <heronboares@gmail.com>
2023-05-18 14:39:00 -0300

    Criação de arquivo core de Model que instancia uma conexão singleton de acordo com as credenciais do arquivo de config, modificação no index para trabalhar com rotas sem views

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+    //Configurações de conexão ao banco de dados
+    const DBDRIVE = 'mysql';
+    const DBHOST = 'localhost';
+    const DBPORT = '3306';
+    const DBNAME = 'CBC_Test_API';
+    const DBUSER = 'root';
+    const DBPASS = '';
