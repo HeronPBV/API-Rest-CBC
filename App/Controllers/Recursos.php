1881dd15275f8733778ce9130a5faa54bda1774f
HeronPBV <heronboares@gmail.com>
2023-05-18 16:55:26 -0300

    Adicionada função que só permite o gasto se houverem recursos suficientes

--- App/Controllers/Recursos.php
+++ App/Controllers/Recursos.php
@@ -14,27 +14,42 @@ class Recursos extends Controller{
         $clube_id = $dadosConsumo->clube_id;
         $recurso_id = $dadosConsumo->recurso_id;
         $valor_consumo = floatval($dadosConsumo->valor_consumo);
-
-        //Verificar antes se há recursos suficientes
-
         $clubeModel->listAll($clube_id);
         $recursoModel->select($recurso_id);
-        $saldo_anterior = $clubeModel->saldo_disponivel;
 
-        if($recursoModel->consume($clubeModel, $valor_consumo)){
-            http_response_code(200);
-            echo json_encode([
-                "clube" => $clubeModel->clube,
-                "saldo_anterior" => $saldo_anterior,
-                "saldo_atual" => $clubeModel->saldo_disponivel
-            ]);
+        if($this->HasEnoughResorces($clubeModel, $recursoModel, $valor_consumo)){
+
+            $saldo_anterior = $clubeModel->saldo_disponivel;
+
+            if($recursoModel->consume($clubeModel, $valor_consumo)){
+                http_response_code(200);
+                echo json_encode([
+                    "clube" => $clubeModel->clube,
+                    "saldo_anterior" => $saldo_anterior,
+                    "saldo_atual" => $clubeModel->saldo_disponivel
+                ]);
+            }else{
+                http_response_code(500);
+                echo json_encode(["erro" => "Problemas ao consumir o recurso"]);
+            }
+
         }else{
             http_response_code(500);
-            echo json_encode(["erro" => "Problemas ao consumir o recurso"]);
+            echo json_encode(["erro" => "Não há recursos suficientes"]);
         }
-
-        
+    
         
     }
+
+    private function HasEnoughResorces(Clube $clube, Recurso $recurso, float $valor_consumo){
+
+        if($recurso->saldo_disponivel >= $valor_consumo && $clube->saldo_disponivel >= $valor_consumo){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
 
 }
